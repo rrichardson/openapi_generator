b14dc5a9eb172e1e2829871c9835801b40a19ba8
Gérald Lelong <gerald.lelong@easymov.fr>
2019-11-18 12:43:24 +0100

    initial commit

--- generated_server/src/main.rs
+++ generated_server/src/main.rs
@@ -1,137 +1,184 @@
 #![allow(dead_code, unused_variables)]
 
 use actix_web::{web::*, App, HttpResponse, HttpServer, Responder};
+use std::error::Error;
 
 pub mod components {
     pub mod schemas {
-        use serde::Deserialize;
+        use serde::{Deserialize, Serialize};
 
-        #[derive(Deserialize)]
+        #[derive(Deserialize, Serialize)]
         pub struct Error {
             pub code: Option<i32>,
             pub message: Option<String>,
         }
 
-        #[derive(Deserialize)]
+        #[derive(Deserialize, Serialize)]
         pub struct Pet {
             pub id: Option<i64>,
             pub name: Option<String>,
             pub tag: Option<String>,
         }
 
-        #[derive(Deserialize)]
+        #[derive(Deserialize, Serialize)]
         pub struct Pets {
-            pub data: Option,
-        }
-
-        #[derive(Deserialize)]
-        pub struct Pets {
-            pub data: Option<Vec<PetsItem>>,
+            pub data: Vec<crate::components::schemas::Pet>,
         }
     }
 }
 
 pub mod list_pets {
-    use super::components;
-    use serde::Deserialize;
+    use actix_web::HttpResponse;
+    use serde::{Deserialize, Serialize};
 
+    /// Parameters for list_pets operation
     #[derive(Deserialize)]
-    pub struct Limit {
-        pub data: Option,
+    pub struct Parameters {
+        /// How many items to return at one time (max 100)
+        pub limit: i32,
     }
 
-    #[derive(Deserialize)]
-    pub struct Limit {
-        pub data: Option<Vec<LimitItem>>,
+    impl Parameters {
+        pub fn new(query: Query, path: Path) -> Self {
+            Self { limit: query.limit }
+        }
     }
 
+    /// Query parameters for list_pets operation
     #[derive(Deserialize)]
     pub struct Query {
         /// How many items to return at one time (max 100)
-        pub limit: Option<Limit>,
+        pub limit: i32,
     }
 
+    /// Path parameters for list_pets operation
     #[derive(Deserialize)]
-    pub struct Response200 {
-        pub data: Option<components::schemas::Pets>,
+    pub struct Path {}
+
+    #[derive(Serialize)]
+    pub enum Response {
+        Response200(Response200),
+        Unspecified(HttpResponse),
     }
 
-    #[derive(Deserialize)]
-    pub struct Response404 {
-        pub data: Option,
-    }
-
-    #[derive(Deserialize)]
-    pub struct ResponseDefault {
-        pub data: Option<components::schemas::Error>,
-    }
+    /// A paged array of pets
+    pub type Response200 = crate::components::schemas::Pets;
 }
 
 pub mod create_pets {
-    use super::components;
-    use serde::Deserialize;
+    use actix_web::HttpResponse;
+    use serde::{Deserialize, Serialize};
 
-    #[derive(Deserialize)]
-    pub struct Query {}
-
-    #[derive(Deserialize)]
-    pub struct Response201 {
-        pub data: Option,
+    #[derive(Serialize)]
+    pub enum Response {
+        Response201(Response201),
+        Unspecified(HttpResponse),
     }
 
-    #[derive(Deserialize)]
-    pub struct ResponseDefault {
-        pub data: Option<components::schemas::Error>,
-    }
+    /// Null response
+    #[derive(Deserialize, Serialize)]
+    pub struct Response201;
 }
 
 pub mod show_pet_by_id {
-    use super::components;
-    use serde::Deserialize;
+    use actix_web::HttpResponse;
+    use serde::{Deserialize, Serialize};
 
+    /// Parameters for show_pet_by_id operation
     #[derive(Deserialize)]
-    pub struct PetId {
-        pub data: Option<String>,
+    pub struct Parameters {
+        /// The id of the pet to retrieve
+        pub pet_id: String,
     }
 
+    impl Parameters {
+        pub fn new(query: Query, path: Path) -> Self {
+            Self {
+                pet_id: path.pet_id,
+            }
+        }
+    }
+
+    /// Query parameters for show_pet_by_id operation
     #[derive(Deserialize)]
     pub struct Query {}
 
+    /// Path parameters for show_pet_by_id operation
     #[derive(Deserialize)]
-    pub struct Response200 {
-        pub data: Option<components::schemas::Pet>,
+    pub struct Path {
+        /// The id of the pet to retrieve
+        pub pet_id: String,
     }
 
-    #[derive(Deserialize)]
-    pub struct ResponseDefault {
-        pub data: Option<components::schemas::Error>,
+    #[derive(Serialize)]
+    pub enum Response {
+        Response200(Response200),
+        Unspecified(HttpResponse),
+    }
+
+    /// Expected response to a valid request
+    pub type Response200 = crate::components::schemas::Pet;
+}
+
+pub trait SwaggerPetstore: Clone {
+    type Error: std::error::Error;
+
+    fn list_pets(
+        &self,
+        parameters: list_pets::Parameters,
+    ) -> Result<list_pets::Response, Self::Error> {
+        unimplemented!()
+    }
+
+    fn show_pet_by_id(
+        &self,
+        parameters: show_pet_by_id::Parameters,
+    ) -> Result<show_pet_by_id::Response, Self::Error> {
+        unimplemented!()
     }
 }
 
 /// List all pets
-fn list_pets(query: Query<list_pets::Query>) -> impl Responder {
-    HttpResponse::Ok().body("List all pets")
-}
-
-/// Create a pet
-fn create_pets() -> impl Responder {
-    HttpResponse::Ok().body("Create a pet")
+fn list_pets<Server: SwaggerPetstore>(
+    server: Data<Server>,
+    query: Query<list_pets::Query>,
+    path: Path<list_pets::Path>,
+) -> impl Responder {
+    use list_pets::*;
+    let parameters = Parameters::new(query.into_inner(), path.into_inner());
+    match server.list_pets(parameters) {
+        Ok(response) => HttpResponse::Ok().body("List all pets"),
+        Err(err) => HttpResponse::InternalServerError().body(err.description().to_string()),
+    }
 }
 
 /// Info for a specific pet
-fn show_pet_by_id(query: Query<show_pet_by_id::Query>) -> impl Responder {
-    HttpResponse::Ok().body("Info for a specific pet")
+fn show_pet_by_id<Server: SwaggerPetstore>(
+    server: Data<Server>,
+    query: Query<show_pet_by_id::Query>,
+    path: Path<show_pet_by_id::Path>,
+) -> impl Responder {
+    use show_pet_by_id::*;
+    let parameters = Parameters::new(query.into_inner(), path.into_inner());
+    match server.show_pet_by_id(parameters) {
+        Ok(response) => HttpResponse::Ok().body("Info for a specific pet"),
+        Err(err) => HttpResponse::InternalServerError().body(err.description().to_string()),
+    }
+}
+
+#[derive(Clone)]
+struct Server;
+impl SwaggerPetstore for Server {
+    type Error = std::io::Error;
 }
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let server = Server {};
+    HttpServer::new(move || {
         App::new()
-            .service(
-                resource("/pets")
-                    .route(get().to(list_pets))
-                    .route(post().to(create_pets)),
-            )
-            .service(resource("/pets/{petId}").route(get().to(show_pet_by_id)))
+            .data(server.clone())
+            .service(resource("/pets").route(get().to(list_pets::<Server>)))
+            .service(resource("/pets/{petId}").route(get().to(show_pet_by_id::<Server>)))
     })
     .bind("127.0.0.1:8080")?
     .run()
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use handlebars::handlebars_helper;
-use handlebars::{
-    Context as HandlebarsContext, Handlebars, Helper, JsonRender, Output, RenderContext,
-    RenderError,
-};
+use handlebars::{Context, Handlebars, Helper, JsonRender, Output, RenderContext, RenderError};
 use json_pointer::JsonPointer;
 use std::fs::File;
 
@@ -13,6 +10,7 @@ fn parse_component_path(ref_path: &str) -> String {
     while let Some(segment) = pointer.pop() {
         path.push(segment);
     }
+    path.push("crate".to_string());
     path.reverse();
     path.join("::")
 }
@@ -35,7 +33,7 @@ macro_rules! case_helper {
         fn $name(
             helper: &Helper,
             _: &Handlebars,
-            _: &HandlebarsContext,
+            _: &Context,
             _: &mut RenderContext,
             out: &mut dyn Output,
         ) -> Result<(), RenderError> {
@@ -44,9 +42,8 @@ macro_rules! case_helper {
                 .params()
                 .iter()
                 .map(|v| v.value().render())
-                .map(|v| v.to_snake_case())
                 .collect::<Vec<_>>();
-            let rendered = values.as_slice().join("_").$function();
+            let rendered = values.as_slice().join(" ").$function();
             out.write(rendered.as_ref())?;
             Ok(())
         }
@@ -60,9 +57,9 @@ handlebars_helper!(component_path: |ref_path: str| parse_component_path(ref_path
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
-    let specs = openapi::from_path("openapi.yaml")
-        .map_err(failure::Fail::compat)
-        .context("failed to parse openapi.yaml")?;
+    // let specs = openapi::from_path("openapi.yaml")
+    //     .map_err(failure::Fail::compat)
+    //     .context("failed to parse openapi.yaml")?;
     let mut handlebars = Handlebars::new();
     handlebars.register_helper("camelcase", Box::new(camelcase));
     handlebars.register_helper("snakecase", Box::new(snakecase));
@@ -72,8 +69,13 @@ fn main() -> Result<()> {
     handlebars.register_template_file("schema", "template/schema.rs.hbs")?;
     handlebars.register_template_file("operation_types", "template/operation_types.rs.hbs")?;
     handlebars.register_template_file("data_type", "template/data_type.rs.hbs")?;
+    handlebars.register_template_file("subtypes", "template/subtypes.rs.hbs")?;
+    handlebars.register_template_file("parameter_type", "template/parameter_type.rs.hbs")?;
     let mut output_file = File::create("generated_server/src/main.rs")?;
-    println!("{}", serde_json::to_string(&specs).unwrap());
-    handlebars.render_to_write("main.rs", &specs, &mut output_file)?;
+    let specs_string = std::fs::read_to_string("openapi.yaml")?;
+    println!("{}", specs_string);
+    let specs_json: serde_yaml::Value = serde_yaml::from_str(&specs_string)?;
+    // println!("{}", serde_json::to_string(&specs).unwrap());
+    handlebars.render_to_write("main.rs", &specs_json, &mut output_file)?;
     Ok(())
 }
