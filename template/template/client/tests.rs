f4c11007c1a557c474b8911b19f8fea60fcdbbbc
Noël Martignoni <noel.martignoni@easymov.fr>
2019-11-30 17:27:58 +0100

    testing with body (wip)

--- template/template/client/tests.rs
+++ template/template/client/tests.rs
@@ -7,5 +7,6 @@ use crate::openapi_serialization::OpenapiSerialization;
 {{#each paths}}
   {{#with get}}{{> test_operation_client operation_verb="get"}}{{~/with}}
   {{#with post}}{{> test_operation_client operation_verb="post"}}{{~/with}}
+  {{#with put}}{{> test_operation_client operation_verb="put"}}{{~/with}}
   {{#with delete}}{{> test_operation_client operation_verb="delete"}}{{~/with}}
 {{~/each}}
