4daa9c694b3eaf1137289dc44174c196e3bd5e38
Noël Martignoni <noel.martignoni@easymov.fr>
2019-12-01 17:17:41 +0100

    schema example

--- template/template/models/mod.rs
+++ template/template/models/mod.rs
@@ -5,6 +5,8 @@ pub mod components {
     pub mod schemas {
         use super::super::components;
         use serde::{Deserialize, Serialize};
+        use serde_json::{json, value::Value};
+
 {{~#each schemas}}
 
 {{>schema name=@key this}}
