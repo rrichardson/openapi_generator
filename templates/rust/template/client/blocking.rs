ee55c9d830dbaa380b4709d8c35d8cfcb6bd9c8d
Noël Martignoni <noel.martignoni@easymov.fr>
2019-12-10 16:47:04 +0100

    no parameters if no parameters

--- templates/rust/template/client/blocking.rs
+++ templates/rust/template/client/blocking.rs
@@ -9,8 +9,8 @@ pub struct {{camelcase info.title "Client"}} {
 
 {{~#*inline "operation_fn"}}
 
-    pub fn {{snakecase operationId}}(&self, {{~#if (not (or parameters requestBody))~}}_{{~/if}}parameters: {{~#if (or parameters requestBody)~}}&{{~/if}}{{snakecase operationId}}::Parameters) -> Result<{{snakecase operationId}}::Response<surf::Response>, surf::Exception> {
-        block_on(self.client.{{snakecase operationId}}({{~#if (or parameters requestBody)~}}parameters{{~else~}}(){{~/if}}))
+    pub fn {{snakecase operationId}}(&self{{~#if (or parameters requestBody)~}}, parameters: &{{snakecase operationId}}::Parameters{{~/if}}) -> Result<{{snakecase operationId}}::Response<surf::Response>, surf::Exception> {
+        block_on(self.client.{{snakecase operationId}}({{~#if (or parameters requestBody)~}}parameters{{~/if}}))
     }
 {{~/inline}}
 
