877776256f225fd4ae2f9a13ded685db1896efa1
Gérald Lelong <gerald.lelong@easymov.fr>
2020-01-03 18:23:52 +0100

    upgrade actix to 2.0.0

--- templates/rust/template/main.rs
+++ templates/rust/template/main.rs
@@ -7,9 +7,11 @@ impl {{camelcase info.title}} for Server {
     type Error = std::io::Error;
 }
 
-fn main() -> std::io::Result<()> {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     let server = Server {};
     HttpServer::new(move || App::new().data(server.clone()).configure(config::<Server>))
     .bind("127.0.0.1:8080")?
     .run()
+    .await
 }
